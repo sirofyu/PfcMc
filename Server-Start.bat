f8f46795ce56b22c1546a8fcc326b73d430bc8bf
sirofyu <sirofyuu46@gmail.com>
2023-03-13 03:37:19 +0900

    test

---
+++ Server-Start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.7.10.jar
+pause
---
+++ 新しいテキスト ドキュメント.bat
@@ -0,0 +1,17 @@
+@echo off
+echo This script will add, commit, and push your changes to GitHub.
+
+REM git add -Aは全ての変更をステージングエリアに追加します
+git add -A
+
+echo Enter the commit message:
+set /p commitmsg=
+
+REM 変数commitmsgをコミットメッセージとして使用します
+git commit -m "%commitmsg%"
+
+REM 変数commitmsgを使って、リモートリポジトリに変更をプッシュします
+git push -u origin main
+
+echo Done!
+pause
